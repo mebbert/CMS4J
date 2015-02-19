346d200534f95bc54169c93f0611487f4b7bc38a
Hayden Smith <haydensmith36@gmail.com>
2015-02-19 12:49:01 -0700

    adding libraries

--- src/cms/Analysis.java
+++ src/cms/Analysis.java
@@ -1,12 +1,25 @@
 package cms;
 
 import java.util.List;
+import org.apache.commons.math3.distribution.*;
+
+
 
 import tools.WindowStats;
 
 public class Analysis {
 	
-	public Analysis() {}
+	private NormalDistribution nd;
+	private GammaDistribution gd;
+	private UniformRealDistribution urd;
+	
+	public Analysis() {
+		
+		nd = new NormalDistribution(0, 1); 		//defined
+		gd = new GammaDistribution(1, 1);			//made up; plot(density(qgamma(pnorm(var), shape=1, rate=1)))
+		urd = new UniformRealDistribution(-1, 1);	//defined
+		
+	}
 	
 	public void runCmsAnalysis(List<WindowStats> all_ws) {
 		
@@ -22,6 +35,9 @@ public class Analysis {
 		//			-do the ¹ function to incorporate all the stats into one CMS statistic
 		//				just multiply the 3 probabilities together
 		//			-NOTE: I don't need ALL 5 stats to do the CMS analysis
+		
+		
+		
 		
 		
 	}
